934126e4ca90b2bf82a244e1398aa2391e8ea303
tywhisky <mikoto221@qq.com>
2020-08-28 20:49:53 +0800

    first

---
+++ src/test/java/com/taine/travels/TravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taine.travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+}
